610fb1a84c858faa995bb735c664b0be2a4c170e
Gil <gil@civity.nl>
2019-03-13 12:57:27 +0100

    Add Email Signature

--- ckanext/definitions/constants.py
+++ ckanext/definitions/constants.py
@@ -1,6 +1,22 @@
+# TODO add Loes Link in HTML
+#                   'Voor meer instructies, is er een module beschikbaar in Loes --> https://gemeentedenhaag.trainingscatalogus.nl/nl/app#/course/76d0bb0e-cec9-4901-9391-bc58bfcf8e2d\n\n' \
+
+EMAIL_SIGNATURE = '\n\n\n' \
+                  'Met vriendelijke groet,\n\n' \
+                  'Functioneel Beheer Datacatalogus\n' \
+                  'databibliotheek@denhaag.nl\n\n' \
+                  '' \
+                  'Gemeente Den Haag\n' \
+                  'Bedrijfsvoeringsexpertisecentrum (BEC)\n' \
+                  'Spui 70\n' \
+                  '2511 BT Den Haag\n' \
+                  'www.denhaag.nl\n\n' \
+                  '' \
+                  'De disclaimer van toepassing op e-mail van de gemeente Den Haag vindt u op: http://www.denhaag.nl/disclaimer'
+
 DELETE_DEFINITION_EMAIL = {"subject": "Gelinkte definitie verwijderd",
                            "message": "Beste collega,\n\n"
                                       "De definitie '{0}' die gelinkt was aan "
                                       "de vermelding '{1}', waarvan u de eigenaar of gemandateerde bent, is gedeactiveerd c.q. verwijderd.\n"
-                                      "Link naar vermelding --> {2}"
+                                      "Link naar vermelding --> {2}" + EMAIL_SIGNATURE
                            }
