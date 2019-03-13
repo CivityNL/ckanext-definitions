b36c6237539a1d28b9dbe31addcb2936026cc127
Gil <gil@civity.nl>
2019-03-13 15:12:53 +0100

    Change Email to text/html

--- ckanext/definitions/constants.py
+++ ckanext/definitions/constants.py
@@ -1,22 +1,23 @@
 # TODO add Loes Link in HTML
-#                   'Voor meer instructies, is er een module beschikbaar in Loes --> https://gemeentedenhaag.trainingscatalogus.nl/nl/app#/course/76d0bb0e-cec9-4901-9391-bc58bfcf8e2d\n\n' \
+#                   'Voor meer instructies, is er een module beschikbaar in Loes --> https://gemeentedenhaag.trainingscatalogus.nl/nl/app#/course/76d0bb0e-cec9-4901-9391-bc58bfcf8e2d<br><br>' \
 
-EMAIL_SIGNATURE = '\n\n\n' \
-                  'Met vriendelijke groet,\n\n' \
-                  'Functioneel Beheer Datacatalogus\n' \
-                  'databibliotheek@denhaag.nl\n\n' \
+EMAIL_SIGNATURE = '<br><br><br>' \
+                  'Voor meer instructies, is er een module beschikbaar in <a href="https://gemeentedenhaag.trainingscatalogus.nl/nl/app#/course/76d0bb0e-cec9-4901-9391-bc58bfcf8e2d">Loes</a> <br><br>' \
+                  'Met vriendelijke groet,<br><br>' \
+                  'Functioneel Beheer Datacatalogus<br>' \
+                  'databibliotheek@denhaag.nl<br><br>' \
                   '' \
-                  'Gemeente Den Haag\n' \
-                  'Bedrijfsvoeringsexpertisecentrum (BEC)\n' \
-                  'Spui 70\n' \
-                  '2511 BT Den Haag\n' \
-                  'www.denhaag.nl\n\n' \
+                  'Gemeente Den Haag<br>' \
+                  'Bedrijfsvoeringsexpertisecentrum (BEC)<br>' \
+                  'Spui 70<br>' \
+                  '2511 BT Den Haag<br>' \
+                  'www.denhaag.nl<br><br>' \
                   '' \
                   'De disclaimer van toepassing op e-mail van de gemeente Den Haag vindt u op: http://www.denhaag.nl/disclaimer'
 
 DELETE_DEFINITION_EMAIL = {"subject": "Gelinkte definitie verwijderd",
-                           "message": "Beste collega,\n\n"
+                           "message": "Beste collega,<br><br>"
                                       "De definitie '{0}' die gelinkt was aan "
-                                      "de vermelding '{1}', waarvan u de eigenaar of gemandateerde bent, is gedeactiveerd c.q. verwijderd.\n"
+                                      "de vermelding '{1}', waarvan u de eigenaar of gemandateerde bent, is gedeactiveerd c.q. verwijderd.<br>"
                                       "Link naar vermelding --> {2}" + EMAIL_SIGNATURE
                            }
